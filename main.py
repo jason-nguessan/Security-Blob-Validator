6f8641dfa85aab3b823dbe2b64d3787d03a78cd5
jason-nguessan <nuthsaid@outlook.com>
2021-09-30 18:52:44 -0400

    get keyvault secret

--- main.py
+++ main.py
@@ -3,32 +3,61 @@
 
 # https://storage12345storage12345.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2021-10-01T04:05:17Z&st=2021-09-30T20:05:17Z&spr=https&sig=YLSBxcVv4qviPxVcZhX9q1cQq2%2FBgHg2Z6Mw5CLvyRM%3D
 
+from azure.storage.blob import BlobServiceClient
 import os
 import uuid
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 
+# sudo -s pip3.9 install azure.storage.blob
+
+# pip3.9 install azure-keyvault-secrets
+
+# pip3.9 install azure-identity
+
+# az login
+
+# replace kVUri with the uri of your keyvault
+
+# replace the sas url, and token with your own
 class AzureValidator:
 
     def __init__(self, url, shared_access_key):
 
-        self.url = url
-        self.shared_access_key = shared_access_key
+        # BEST TO EXPORT TO PATH os.environ["KEY_VAULT_NAME"]
+        keyVaultName = "pytestKeyVault"
+        KVUri = f"https://{keyVaultName}.vault.azure.net"
+
         try:
+            client = SecretClient(
+                vault_url=KVUri, credential=DefaultAzureCredential()).get_secret("secretPassword")
+        except Exception as ex:
+            print(ex)
+
+        try:
+
             print("Azure Blob Storage v" + __version__ +
                   " - Python quickstart sample")
-            blobServiceClient = BlobServiceClient(
-                self.url, credential=self.shared_access_key)
+            self.blobServiceClient = BlobServiceClient(
+                url, credential=shared_access_key)
 
-            container_client = blobServiceClient.list_containers()
+            container_client = self.blobServiceClient.list_containers()
             for blob in container_client:
                 print(blob.name)
 
-            # Quick start code goes here
+                # Quick start code goes here
 
         except Exception as ex:
             print('Exceptihon:')
             print(ex)
+
+    
+    def _checkBlobName(self) -> bool:
+        container_client = self.blobServiceClient.list_containers()
+        for blob in container_client:
+            print(blob.name)
 
 
 def func(x):
@@ -43,3 +72,11 @@ azureValidator = AzureValidator(
     "https://storage12345storage12345.blob.core.windows.net/", "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-10-01T04:24:45Z&st=2021-09-30T20:24:45Z&spr=https,http&sig=xI%2BvVK9n%2BsyCmCho3sAfnxiWhL2XR72p8ACq7tdvpYc%3D")
 
 # test_answer()
+
+
+# connection_string = ''
+# blob_service_client = BlobServiceClient.from_connection_string(
+#     connection_string)
+# container_client = blob_service_client.get_container_client("<container name>")
+# blob_client = container_client.get_blob_client("<blob name>")
+# blob_client.download_blob().readall()  # read blob content as string
