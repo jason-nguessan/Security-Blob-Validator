0565c4cf032a889784e616b750f0bb0f5e26266c
jason-nguessan <nuthsaid@outlook.com>
2021-10-01 13:06:14 -0400

    pytest + a couple inline documentations

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-
-# BlobEndpoint=https://storage12345storage12345.blob.core.windows.net/;QueueEndpoint=https://storage12345storage12345.queue.core.windows.net/;FileEndpoint=https://storage12345storage12345.file.core.windows.net/;TableEndpoint=https://storage12345storage12345.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2021-10-01T04:05:17Z&st=2021-09-30T20:05:17Z&spr=https&sig=YLSBxcVv4qviPxVcZhX9q1cQq2%2FBgHg2Z6Mw5CLvyRM%3D
-
-# https://storage12345storage12345.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2021-10-01T04:05:17Z&st=2021-09-30T20:05:17Z&spr=https&sig=YLSBxcVv4qviPxVcZhX9q1cQq2%2FBgHg2Z6Mw5CLvyRM%3D
-
 from azure.storage.blob import BlobServiceClient
 import os
 import uuid
@@ -11,20 +6,9 @@ from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
 
-# sudo -s pip3.9 install azure.storage.blob
+class AzureSecretValidator:
 
-# pip3.9 install azure-keyvault-secrets
-
-# pip3.9 install azure-identity
-
-# az login
-
-# replace kVUri with the uri of your keyvault
-
-# replace the sas url, and token with your own
-class AzureValidator:
-
-    def __init__(self, storageAccountUrl, sharedAccessKey, kvUrl, secretPassword):
+    def __init__(self, storageAccountUrl, sharedAccessKey, kvUrl, secretName):
 
         # BEST TO EXPORT TO PATH os.environ["KEY_VAULT_NAME"]
 
@@ -32,10 +16,9 @@ class AzureValidator:
         if(not self.blobServiceClient or not self.keyVaultClient):
             return
 
-        print(self.keyVaultClient.get_secret(secretPassword).value)
+        self.secret = self.keyVaultClient.get_secret(secretName).value
 
     def _connectToClients(self, storageAccountUrl, sharedAccessKey, kvUrl):
-        print("")
 
         try:
             self.keyVaultClient = SecretClient(
@@ -43,38 +26,59 @@ class AzureValidator:
             self.blobServiceClient = BlobServiceClient(
                 storageAccountUrl, credential=sharedAccessKey)
             print("----------\n Successfully Signed in \n----------")
+            print(
+                "----------\n Throws Warning if Keyvault is not exported to OS \n----------")
         except Exception as ex:
             print(ex)
             self.keyVaultClient = None
             self.blobServiceClient = None
 
-    def _checkBlobName(self) -> bool:
-        container_client = self.blobServiceClient.list_containers()
-        for blob in container_client:
-            print(blob.name)
+    def isSecretInBlobName(self) -> bool:
+        for blob in self.blobServiceClient.list_containers():
+            if(self.secret in blob.name):
+                print(
+                    "----------\n Breach!!!! Secret was Found in Container's Name \n----------")
+                return True
+        return False
 
 
-def func(x):
-    return x + 1
+# STEP 1 - Run these commands in super use mode before running these commands  - python 3.9.7
+
+        # az login
+
+        # pip3.9 install azure.storage.blob
+
+        # pip3.9 install azure-keyvault-secrets
+
+        # pip3.9 install azure-identity
+
+        # pip3.9 install pytest
 
 
-def test_answer():
-    assert func(3) == 5
+# STEP 2 - Replace the following values with your own  - Valid untill 01/01/2022
+
+# storage account url
+storageAccountUrl = "https://storage12345storage12345.blob.core.windows.net/"
+
+# shared access key, also known as shared access token
+sharedAccessKey = "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-10-16T00:44:05Z&st=2021-10-01T16:44:05Z&spr=https&sig=y3TJpZOx%2BVOz6xNLuBG7p%2BONIWcSBx4C1BO5M8G9LA0%3D"
 
 
-azureValidator = AzureValidator(
-    "https://storage12345storage12345.blob.core.windows.net/",
-    "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-10-01T04:24:45Z&st=2021-09-30T20:24:45Z&spr=https,http&sig=xI%2BvVK9n%2BsyCmCho3sAfnxiWhL2XR72p8ACq7tdvpYc%3D",
-    f"https://pytestkeyvault.vault.azure.net",
-    "secretPassword"
-)
+# key vault url
+kvUrl = f"https://pytestkeyvault.vault.azure.net"
 
-# test_answer()
+# key vault secret name
+secretName = "secretPassword1"
 
 
-# connection_string = ''
-# blob_service_client = BlobServiceClient.from_connection_string(
-#     connection_string)
-# container_client = blob_service_client.get_container_client("<container name>")
-# blob_client = container_client.get_blob_client("<blob name>")
-# blob_client.download_blob().readall()  # read blob content as string
+# STEP 3 - Run the command
+
+# python3.9 main.py
+
+azureValidator = AzureSecretValidator(
+    storageAccountUrl, sharedAccessKey, kvUrl, secretName)
+
+
+assert(azureValidator.isSecretInBlobName() == False)
+
+
--- rg_pytest.tf
+++ rg_pytest.tf
@@ -9,7 +9,18 @@ resource "azurerm_resource_group" "pytest_rg" {
 }
 
 
+
+#STEP 1 
+  # Uncomment the following and create a key vault
+  
+  # Once done, comment it or delete it
+  
+
+/*
+
 #Pytest Keyvault
+
+# Create your Key vault and assign a password
 resource "azurerm_key_vault" "pytest_kv" {
 
   #Reequired
@@ -39,8 +50,37 @@ resource "azurerm_key_vault" "pytest_kv" {
   }
 }
 
+#Please create your secret outside of terraform or do it here (not - recommended)
 
-#storage12345_sa
+*/
+
+#Step 2 - Uncomment the rest
+
+/*
+
+variable nContainers {
+  type        = number
+  default     = 1500
+  description = "This creates N numbers of containers - USE AT YOUR OWN RISK"
+}
+
+
+
+#Pytestkeyvault
+data "azurerm_key_vault" "pytest_kv" {
+  name = "pytestKeyVault"
+  resource_group_name = azurerm_resource_group.pytest_rg.name
+}
+
+
+#Pytest vault secret
+data "azurerm_key_vault_secret" "secretPassword1" {
+  name         = "secretPassword1"
+  key_vault_id =  data.azurerm_key_vault.pytest_kv.id
+}
+
+
+#pytest storage account 
 resource "azurerm_storage_account" "storage12345_sa" {
   name                     = "storage12345storage12345"
   resource_group_name      = azurerm_resource_group.pytest_rg.name
@@ -54,9 +94,16 @@ resource "azurerm_storage_account" "storage12345_sa" {
 }
 
 
+#pytest storage container
+
+
+
 resource "azurerm_storage_container" "storage1_container" {
-  name                  = "file"
+  count                 = var.nContainers
+  name                  =  count.index % 5 == 0  ?"file${count.index}" : lower("${data.azurerm_key_vault_secret.secretPassword1.value}${count.index}")
   storage_account_name  = azurerm_storage_account.storage12345_sa.name
   container_access_type = "private"
 
 }
+
+*/
